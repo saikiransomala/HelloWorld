133f9a9c17d61925f077800aa6aafa328d810062
saikiran email = saikiran11.somala@gmail.com <slk@WSLK1FFTRFW-24.SLKSOFT.COM>
2018-09-02 13:00:43 +0530

    commited modifications

--- HelloWorld/src/slk/train/HelloWorld.java
+++ HelloWorld/src/slk/train/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("I am Sai");
-		System.out.println("Pushing modification to GIT.");
+		System.out.println("Pushing modification to GIT");
 	}
 
 }
